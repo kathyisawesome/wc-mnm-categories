9fa657b6f3adc555fdefce0278b519cb0c8eab32
Kathy Daring <helgatheviking@gmail.com>
2019-11-29 09:40:09 -0500

    Make categories sortable, for some reason WC core doesn't handle this.

--- assets/js/wc-mnm-categories-metabox.js
+++ assets/js/wc-mnm-categories-metabox.js
@@ -14,6 +14,27 @@ jQuery( document ).ready( function( $ ) {
 
 	$( "#mnm_product_data input.mnm_use_category:checked" ).change();
 
-} );
+	$( '.wc-category-search' ).filter( '.enhanced' ).each( function() {
 
+		if ( $( this ).data( 'sortable' ) ) {
+			var $select = $(this);
+			var $list   = $( this ).next( '.select2-container' ).find( 'ul.select2-selection__rendered' );
+
+			$list.sortable({
+				placeholder : 'ui-state-highlight select2-selection__choice',
+				forcePlaceholderSize: true,
+				items       : 'li:not(.select2-search__field)',
+				tolerance   : 'pointer',
+				stop: function() {
+					$( $list.find( '.select2-selection__choice' ).get().reverse() ).each( function() {
+						var id     = $( this ).data( 'data' ).id;
+						var option = $select.find( 'option[value="' + id + '"]' )[0];
+						$select.prepend( option );
+					} );
+				}
+			});
+		}
+	});
+
+});
 
