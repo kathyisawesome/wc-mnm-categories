b6ebd18e22dbfee7bbb27ef6548c6857c64f7b69
Kathy <507025+helgatheviking@users.noreply.github.com>
2022-02-25 12:00:48 -0700

    JShint fixups

--- assets/js/admin/wc-mnm-categories-metabox.js
+++ assets/js/admin/wc-mnm-categories-metabox.js
@@ -1,18 +1,18 @@
 jQuery( document ).ready( function( $ ) {
 
-	$( "#mnm_product_data input.mnm_use_category" ).on( 'change', function() {
+	$( '#mnm_product_data input.mnm_use_category' ).on( 'change', function() {
 
 		if( $( this ).val() === 'yes' ) {
-			$( "#mnm_allowed_contents_options" ).hide();
-			$( ".mnm_product_cat_field" ).show();
+			$( '#mnm_allowed_contents_options' ).hide();
+			$( '.mnm_product_cat_field' ).show();
 		} else {
-			$( "#mnm_allowed_contents_options" ).show();
-			$( ".mnm_product_cat_field" ).hide();
+			$( '#mnm_allowed_contents_options' ).show();
+			$( '.mnm_product_cat_field' ).hide();
 		}
 
 	} );
 
-	$( "#mnm_product_data input.mnm_use_category:checked" ).trigger( 'change' );
+	$( '#mnm_product_data input.mnm_use_category:checked' ).trigger( 'change' );
 
 	$( ':input.wc-mnm-category-search' ).filter( ':not(.enhanced)' ).each( function() {
 
